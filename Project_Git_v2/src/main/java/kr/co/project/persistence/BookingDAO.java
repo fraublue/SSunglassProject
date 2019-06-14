d9a88177caeae63ee87ba35d43dd2119c56f3a69
fraublue <46223940+fraublue@users.noreply.github.com>
2019-06-14 17:30:28 +0900

    booking 다시

--- Project_Git_v2/src/main/java/kr/co/project/persistence/BookingDAO.java
+++ Project_Git_v2/src/main/java/kr/co/project/persistence/BookingDAO.java
@@ -2,15 +2,16 @@ package kr.co.project.persistence;
 
 import java.util.List;
 
+import kr.co.project.domain.BookingListVO;
 import kr.co.project.domain.BookingVO;
-import kr.co.project.domain.CommonBoardVO;
 import kr.co.project.domain.Criteria;
 
 public interface BookingDAO {
 	public void insertBooking(BookingVO vo)throws Exception;
 	public void deleteBooking(BookingVO vo);
 	public boolean checkExistBooking(int board_id, String user_id);
-	public List<CommonBoardVO> bookingList(String user_id,Criteria cri);
+	public List<BookingListVO> tbookingList(String user_id,Criteria cri);
+	public List<BookingListVO> gbookingList(String user_id,Criteria cri);
 	public void deletebooking_boardid(int board_id);
 	public void deletebooking_userid(String user_id);
 }
